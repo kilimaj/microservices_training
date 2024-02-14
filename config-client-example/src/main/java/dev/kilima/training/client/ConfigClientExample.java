c1ec297c2fd9831191709815f26b34805e41e86a
John Kilima <jkbundala@gmail.com>
2024-02-14 14:49:39 +0530

    Step2 - Config client : Create the ConfigClientMain class

---
+++ config-client-example/src/main/java/dev/kilima/training/client/ConfigClientExample.java
@@ -0,0 +1,22 @@
+package dev.kilima.training.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class ConfigClientExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ConfigClientExample.class, args);
+
+	}
+
+	@Autowired
+	public void setEnvironment(Environment env) {
+		System.out.println(env.getProperty("msg"));
+	}
+
+}
