e9ed615ae885cb876e0b7b26c4b6f7940c709b91
John Kilima <jkbundala@gmail.com>
2024-02-15 10:19:36 +0530

    Project Eureka : Create main class

---
+++ eureka-server/src/main/java/dev/kilima/training/eureka/EurekaServerMain.java
@@ -0,0 +1,16 @@
+package dev.kilima.training.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerMain.class, args);
+
+	}
+
+}
