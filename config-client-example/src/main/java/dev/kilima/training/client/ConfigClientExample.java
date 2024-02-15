93a8ea3c610e65d5cb78d14a9d67946d17a30e5c
John Kilima <jkbundala@gmail.com>
2024-02-15 11:36:12 +0530

    Project Eureka : Adding config client to the eureka

--- config-client-example/src/main/java/dev/kilima/training/client/ConfigClientExample.java
+++ config-client-example/src/main/java/dev/kilima/training/client/ConfigClientExample.java
@@ -3,9 +3,11 @@ package dev.kilima.training.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientExample {
 
 	public static void main(String[] args) {
