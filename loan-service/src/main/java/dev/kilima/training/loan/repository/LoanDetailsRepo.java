543bc31f4f6f34b8609789e26ffd7e229ba381f6
John Kilima <jkbundala@gmail.com>
2024-02-15 15:01:18 +0530

    Project Loan Service : Loan Service - Small fix on the Loan Repo for return type

--- loan-service/src/main/java/dev/kilima/training/loan/repository/LoanDetailsRepo.java
+++ loan-service/src/main/java/dev/kilima/training/loan/repository/LoanDetailsRepo.java
@@ -1,5 +1,7 @@
 package dev.kilima.training.loan.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface LoanDetailsRepo extends JpaRepository<LoanDetailsRepo, Long> {
 
 	@Query(value = "select max(loan.loanId) from LoanDetails loan")
-	long getMaxId(long loanId);
+	Optional<Integer> getMaxId(long loanId);
 	
 	@Query(value = "select loan.status from LoanDetails loan where loan.loanId=?1")
 	String checkLoanStatus(long loanId);
