9da59de4003110820a053a2b7fa47cb79c546fa6
John Kilima <jkbundala@gmail.com>
2024-02-16 11:57:32 +0530

    Project Crediscore and Loan service : Create Credit score controller

---
+++ creditscore-service/src/main/java/dev/kilima/training/creditscore/controller/CreditScoreController.java
@@ -0,0 +1,42 @@
+package dev.kilima.training.creditscore.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.kilima.training.creditscore.entity.CreditScore;
+import dev.kilima.training.creditscore.service.CreditScoreService;
+
+@RestController
+@RequestMapping("/creditscore")
+public class CreditScoreController {
+	@Autowired
+	CreditScoreService service;
+
+	@PostMapping
+	public ResponseEntity<String> add(@RequestBody CreditScore score) {
+		service.add(score);
+		return new ResponseEntity<String>("inserted", HttpStatus.OK);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CreditScore>> findAll() {
+		List<CreditScore> list = service.findAll();
+		return new ResponseEntity<List<CreditScore>>(list, HttpStatus.OK);
+	}
+
+	@GetMapping("/{pancard}")
+	public ResponseEntity<Optional<CreditScore>> getCreditScore(@PathVariable String pancard) {
+		Optional<CreditScore> score = service.getCreditScore(pancard);
+		return new ResponseEntity<Optional<CreditScore>>(score, HttpStatus.OK);
+	}
+}
