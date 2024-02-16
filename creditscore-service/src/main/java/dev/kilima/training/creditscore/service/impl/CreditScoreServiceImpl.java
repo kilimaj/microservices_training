e42aded2f516fd9b821d92f43f16d5b43d077b7b
John Kilima <jkbundala@gmail.com>
2024-02-16 11:49:21 +0530

    Project Crediscore and Loan service : Create Credit score service implementation

---
+++ creditscore-service/src/main/java/dev/kilima/training/creditscore/service/impl/CreditScoreServiceImpl.java
@@ -0,0 +1,35 @@
+package dev.kilima.training.creditscore.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.kilima.training.creditscore.entity.CreditScore;
+import dev.kilima.training.creditscore.repository.CreditScoreRepo;
+import dev.kilima.training.creditscore.service.CreditScoreService;
+
+@Service
+public class CreditScoreServiceImpl implements CreditScoreService{
+
+	@Autowired
+	CreditScoreRepo repo;
+	
+	@Override
+	public void add(CreditScore credit) {
+		repo.save(credit);
+	}
+
+	@Override
+	public List<CreditScore> findAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public Optional<CreditScore> getCreditScore(String pancard) {
+		Optional<CreditScore> creditscore = repo.findById(pancard);
+		return creditscore;
+	}
+	
+}
