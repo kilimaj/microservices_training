fc61847f0611ab03a4de10222b2dfa5449577c47
John Kilima <jkbundala@gmail.com>
2024-02-17 10:43:43 +0530

    Project Crediscore and Loan service : Feign Client - Create CreditScore proxy interface

---
+++ loan-service/src/main/java/dev/kilima/training/loan/repository/CreditScoreProxy.java
@@ -0,0 +1,19 @@
+package dev.kilima.training.loan.repository;
+
+import java.util.Optional;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import dev.kilima.training.loan.dto.CreditScore;
+
+/*
+ * The following should be used to send request from Feign Client which includes 
+ * @FeignClient(name="service name", url="that shows on Eureka terminal"
+ */
+@FeignClient(name = "creditscore-service", url = "http://localhost:8092/creditscore")
+public interface CreditScoreProxy {
+	@GetMapping(value = "/{pancard}")
+	Optional<CreditScore> getCreditScore(@PathVariable String pancard);
+}
