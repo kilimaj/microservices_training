9d5c24db1f043ef9fa547c9dbfc7b35fb63d577f
John Kilima <jkbundala@gmail.com>
2024-02-17 10:45:26 +0530

    Project Crediscore and Loan service : Feign Client - Connect by adding the proxy clas to Main class

--- loan-service/src/main/java/dev/kilima/training/loan/LoanServiceMain.java
+++ loan-service/src/main/java/dev/kilima/training/loan/LoanServiceMain.java
@@ -3,11 +3,15 @@ package dev.kilima.training.loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import dev.kilima.training.loan.repository.CreditScoreProxy;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(clients = {CreditScoreProxy.class})
 public class LoanServiceMain {
 
 	public static void main(String[] args) {
