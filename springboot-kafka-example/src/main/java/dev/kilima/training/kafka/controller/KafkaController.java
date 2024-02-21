65742d5a87fa1b83c26d9f666c77e01b98d93fba
John Kilima <jkbundala@gmail.com>
2024-02-21 12:18:37 +0530

    Project Kafka : Create controller class

---
+++ springboot-kafka-example/src/main/java/dev/kilima/training/kafka/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package dev.kilima.training.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.kilima.training.kafka.producer.KafkaSender;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+	@Autowired
+	KafkaSender sender;
+	
+	@GetMapping("/{mesg}")
+	public String producer(@PathVariable String mesg) {
+		sender.send(mesg);
+		return "Message sent to kafka: " + mesg;
+	}
+}
