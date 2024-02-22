724c467f30c77fa4bc9e0df5577dbab3562ec3f6
John Kilima <jkbundala@gmail.com>
2024-02-22 10:27:23 +0530

    Project Redis Cache : Adding main class

---
+++ springboot-redis-session-example/src/main/java/dev/kilima/training/redis/SpringBootSessionRedisMain.java
@@ -0,0 +1,13 @@
+package dev.kilima.training.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionRedisMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionRedisMain.class, args);
+	}
+
+}
